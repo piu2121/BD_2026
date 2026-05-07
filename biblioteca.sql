a1f7c7dcecd2c46dee432b4a7f2cff34f3492418
piu2121 <elias71802@gmail.com>
2026-05-07 18:18:05 -0300

    adicionando triggers

--- biblioteca.sql
+++ biblioteca.sql
@@ -94,4 +94,62 @@ INSERT INTO Livro_Emprestimo(Codigo_Emprestimo, Codigo_Livro) VALUES
 
 10) Select Nome from Usuarios Inner Join  Emprestimo On = where Matricula=Matricula_Usuario and  Data_Hora Between 01/05/2025 and 31/05/2025
 
-11)Select Autor* from Usuarios,Emprestimo,Livro_Emprestimo where  Titulo Like '%Python%'
+11)SELECT  Livro.Autor FROM LivroINNER JOIN Livro_Emprestimo ON Livro.Codigo = Livro_Emprestimo.Codigo_Livro WHERE Livro.Titulo LIKE '%Python%';
+
+--trigger 1
+
+CREATE TRIGGER trg_valida_data_devolucao
+BEFORE INSERT ON Emprestimo
+FOR EACH ROW
+BEGIN
+	--verifica se a data de entrega no livro é um número e se é um número válido
+    -- Só valida se a Data_Devolucao for informada (não nula)
+    IF NEW.Data_Devolucao IS NOT NULL AND NEW.Data_Devolucao <= NEW.Data_Hora THEN
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'Data de devolução deve ser posterior à data do empréstimo.';
+    END IF;
+END;
+
+--trigger 2
+
+CREATE TRIGGER trg_verifica_livro_ja_emprestado
+BEFORE INSERT ON Livro_Emprestimo
+FOR EACH ROW
+BEGIN
+    DECLARE v_count INT;
+	--solta uma mensagem dizendo que este livro já foi pego e não foi devolvido até o presente momento
+    -- Conta se este livro já aparece em um empréstimo que não foi devolvido
+    SELECT COUNT(*)
+    INTO v_count
+    FROM Livro_Emprestimo LE
+    INNER JOIN Emprestimo E ON LE.Codigo_Emprestimo = E.Codigo
+    WHERE LE.Codigo_Livro = NEW.Codigo_Livro
+      AND E.Data_Devolucao IS NULL;
+
+    IF v_count > 0 THEN
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'Este livro já está emprestado e ainda não foi devolvido.';
+    END IF;
+END;
+
+-- trigger 3
+
+CREATE TRIGGER trg_limite_emprestimos_ativos
+BEFORE INSERT ON Emprestimo
+FOR EACH ROW
+BEGIN
+    DECLARE v_ativos INT;
+--não pode passar de 5 livros,se passar cancela na hora o livro que ele está tentado fazer imprestimo
+    -- Conta quantos empréstimos ativos o usuário já possui
+    SELECT COUNT(*)
+    INTO v_ativos
+    FROM Emprestimo
+    WHERE Matricula_Usuario = NEW.Matricula_Usuario
+      AND Data_Devolucao IS NULL;
+
+    IF v_ativos >= 5 THEN
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'Usuário já possui 5 livros emprestados. Devolva algum antes de pegar um novo.';
+    END IF;
+END;
+
